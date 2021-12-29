affe99863d082f03d6629ddb870e0eae9f6dbf51
landalot <xvoxy27@gmail.com>
2021-12-29 00:26:33 -0500

    Indentation setup.

--- Lettershift.py
+++ Lettershift.py
@@ -26,42 +26,42 @@ for char in strin:
         continue
 
 #Setting up a dictionary.
+    else:
+        LetterNumber = {
+            "a":1,
+            "b":2,
+            "c":3,
+            "d":4,
+            "e":5,
+            "f":6,
+            "g":7,
+            "h":8,
+            "i":9,
+            "j":10,
+            "k":11,
+            "l":12,
+            "m":13,
+            "n":14,
+            "o":15,
+            "p":16,
+            "q":17,
+            "r":18,
+            "s":19,
+            "t":20,
+            "u":21,
+            "v":22,
+            "w":23,
+            "x":24,
+            "y":25,
+            "z":26,
+            }
 
-    LetterNumber = {
-        "a":1,
-        "b":2,
-        "c":3,
-        "d":4,
-        "e":5,
-        "f":6,
-        "g":7,
-        "h":8,
-        "i":9,
-        "j":10,
-        "k":11,
-        "l":12,
-        "m":13,
-        "n":14,
-        "o":15,
-        "p":16,
-        "q":17,
-        "r":18,
-        "s":19,
-        "t":20,
-        "u":21,
-        "v":22,
-        "w":23,
-        "x":24,
-        "y":25,
-        "z":26,
-        }
+        #Referencing the dictionary for a character
+        num = LetterNumber.get(char)
+        #Debug print
+        #print(num)
 
-    #Referencing the dictionary for a character
-    num = LetterNumber.get(char)
-    #Debug print
-    #print(num)
-
-    #add test
+        #add test
 
     num = num + 2
 
