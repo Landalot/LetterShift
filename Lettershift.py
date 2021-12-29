4efba844e1079e2e6ee40b7f47cf97270b2d94be
landalot <xvoxy27@gmail.com>
2021-12-29 00:42:40 -0500

    This should make it so that capitals work.
    I committed this using kate git integration.

--- Lettershift.py
+++ Lettershift.py
@@ -22,11 +22,39 @@ for char in strin:
 
     if char == "A" or char == "B" or char == "C" or char == "D" or char == "E" or char == "F" or char == "G" or char == "H" or char == "I" or char == "J" or char == "K" or char == "L" or char == "M" or char == "N" or char == "O" or char == "P" or char == "Q" or char == "R" or char == "S" or char == "T" or char == "U" or char == "V" or char == "W" or char == "X" or char == "Y" or char == "Z":
         print("This is a cap")
-        newstring = addstring("?")
-        continue
+        CCap = True
+        LetterNumber = {
+            "A":1,
+            "B":2,
+            "C":3,
+            "D":4,
+            "E":5,
+            "F":6,
+            "G":7,
+            "H":8,
+            "I":9,
+            "J":10,
+            "K":11,
+            "L":12,
+            "M":13,
+            "N":14,
+            "O":15,
+            "P":16,
+            "Q":17,
+            "R":18,
+            "S":19,
+            "T":20,
+            "U":21,
+            "V":22,
+            "W":23,
+            "X":24,
+            "Y":25,
+            "Z":26,
+            }
 
 #Setting up a dictionary.
     else:
+        CCap = False
         LetterNumber = {
             "a":1,
             "b":2,
@@ -66,34 +94,64 @@ for char in strin:
     num = num + 2
 
     #Setting up reverse dictionary
-    NumtoLet = LetterNumber = {
-        1:"a",
-        2:"b",
-        3:"c",
-        4:"d",
-        5:"e",
-        6:"f",
-        7:"g",
-        8:"h",
-        9:"i",
-        10:"j",
-        11:"k",
-        12:"l",
-        13:"m",
-        14:"n",
-        15:"o",
-        16:"p",
-        17:"q",
-        18:"r",
-        19:"s",
-        20:"t",
-        21:"u",
-        22:"v",
-        23:"w",
-        24:"x",
-        25:"y",
-        26:"z",
-        }
+    if CCap == False:
+        NumtoLet = LetterNumber = {
+            1:"a",
+            2:"b",
+            3:"c",
+            4:"d",
+            5:"e",
+            6:"f",
+            7:"g",
+            8:"h",
+            9:"i",
+            10:"j",
+            11:"k",
+            12:"l",
+            13:"m",
+            14:"n",
+            15:"o",
+            16:"p",
+            17:"q",
+            18:"r",
+            19:"s",
+            20:"t",
+            21:"u",
+            22:"v",
+            23:"w",
+            24:"x",
+            25:"y",
+            26:"z",
+            }
+    If CCap = True:
+        NumtoLet = LetterNumber = {
+            1:"A",
+            2:"B",
+            3:"C",
+            4:"D",
+            5:"E",
+            6:"F",
+            7:"G",
+            8:"H",
+            9:"I",
+            10:"J",
+            11:"K",
+            12:"L",
+            13:"M",
+            14:"N",
+            15:"O",
+            16:"P",
+            17:"Q",
+            18:"R",
+            19:"S",
+            20:"T",
+            21:"U",
+            22:"V",
+            23:"W",
+            24:"X",
+            25:"Y",
+            26:"Z",
+            }
 
     #convert back to letters
     newchar = NumtoLet.get(num)
